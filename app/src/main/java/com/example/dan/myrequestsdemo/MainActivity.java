e36896f1c6916fa4faeea6d43a89dbf873158825
loveping <435675213@qq.com>
2016-10-11 17:50:26 +0800

    关于RXandroid的小实例

--- app/src/main/java/com/example/dan/myrequestsdemo/MainActivity.java
+++ app/src/main/java/com/example/dan/myrequestsdemo/MainActivity.java
@@ -2,18 +2,37 @@ package com.example.dan.myrequestsdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.dan.myrequestsdemo.android_rx.RxUtils;
+
+/**
+ *
+ */
 public class MainActivity extends AppCompatActivity {
 
     private Button click_me_BN;
     private TextView result_TV;
+    private ImageView img_01;
+    private String path = "http://pic33.nipic.com/20130916/3420027_192919547000_2.jpg";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         click_me_BN = (Button) findViewById(R.id.click_me_BN);
         result_TV = (TextView) findViewById(R.id.result_TV);
+        img_01 = (ImageView) findViewById(R.id.img_01);
+
+        click_me_BN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                RxUtils.createObserable();
+               // Toast.makeText(MainActivity.this,"aaa",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/dan/myrequestsdemo/android_rx/RxUtils.java
@@ -0,0 +1,60 @@
+package com.example.dan.myrequestsdemo.android_rx;
+
+
+import android.util.Log;
+
+import rx.Observable;
+import rx.Subscriber;
+
+/**
+ * Created by dan on 2016/10/11.
+ */
+
+public class RxUtils {
+    private static final String TAG = RxUtils.class.getSimpleName();
+
+    public static void createObserable(){
+        //定义被观察者
+        Observable<String> observable = Observable.create(new Observable.OnSubscribe<String>() {
+            @Override
+            public void call(Subscriber<? super String> subscriber) {
+                if (!subscriber.isUnsubscribed()){
+                    subscriber.onNext("hello");
+                    subscriber.onNext("hi");
+                    subscriber.onNext(downloadjson());
+                    subscriber.onNext("world");
+                    subscriber.onCompleted();
+
+                }
+            }
+        });
+
+        Subscriber<String > subscriber = new Subscriber<String>() {
+            @Override
+            public void onCompleted() {
+                Log.i(TAG,"onCompleted");
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.i(TAG,e.getMessage());
+            }
+
+            @Override
+            public void onNext(String s) {
+                Log.i(TAG,"result-->>" +s);
+            }
+        };
+
+        observable.subscribe(subscriber);//关联被观察者
+    }
+
+    /**
+     *
+     * @return
+     * 使用下载方法
+     */
+    public static String downloadjson(){
+        return "json data";
+    }
+}
